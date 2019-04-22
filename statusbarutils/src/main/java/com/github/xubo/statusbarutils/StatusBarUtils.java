978bc96e0ea1abc5ff832ceb6e01e246509b6779
xubo <xubo@baihe.com>
2019-04-22 11:26:43 +0800

    适配各机型的状态栏

--- statusbarutils/src/main/java/com/github/xubo/statusbarutils/StatusBarUtils.java
+++ statusbarutils/src/main/java/com/github/xubo/statusbarutils/StatusBarUtils.java
@@ -151,7 +151,7 @@ public class StatusBarUtils {
      * @param activity
      * @return 设置成功true, 设置失败false
      */
-    public static boolean statusBarTransparen(Activity activity) {
+    public static boolean setStatusBarTransparen(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = activity.getWindow();
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
@@ -177,7 +177,7 @@ public class StatusBarUtils {
      * @param activity
      * @return 设置成功true, 设置失败false
      */
-    public static boolean statusBarTransparenLight(Activity activity) {
+    public static boolean setStatusBarTransparenLight(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             OSInfo.OSType osType = OSInfo.getRomType(activity);
             Window window = activity.getWindow();
